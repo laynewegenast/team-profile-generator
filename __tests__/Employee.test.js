338a180e2b5989259204aaf6fa955bf00bf1d022
macbeth <layne.wegenast@gmail.com>
2022-04-13 19:08:25 -0500

    employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,28 @@
-const { expect } = require('@jest/globals');
-const { test } = require('picomatch');
 const Employee = require('../lib/Employee');
 
 test('creates employee object', () => {
-    const employee = new Employee('sample', 111, 'sample@gmail.com');
+    const employee = new Employee('Jared', 111, 'sample@gmail.com');
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+test('gets employee name', () => {
+    const employee = new Employee ('Jared', 111, 'sample@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('get employee id', () => {
+    const employee = new Employee('Jared', 111, 'sample@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('gets employee email', () => {
+    const employee = new Employee('Jared', 111, 'sample@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
